508b336fd1898c69d6522461af3cad24d580e67f
motypes <mo@openfren.com>
2023-04-27 13:45:57 +0800

    fixed bugs with js files and errors with chatgpt createcompletions api

--- azure/src/functions/getChatGPTSuggestion.js
+++ azure/src/functions/getChatGPTSuggestion.js
@@ -1,14 +1,19 @@
-const { app } = require('@azure/functions');
-const openai = require("../../../lib/fetchSuggestionFromChatGPT")
+const { app } = require("@azure/functions");
+const openai = require("../../lib/openai");
 
-app.http('getChatGPTSuggestion', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
+app.http("getChatGPTSuggestion", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Create a random text prompt for DALL-E to generate an image, this prompt will be shown to the user,
+            include details such as the genre, what the type of painting it should be, example options can be: 
+            oil painting, watercolor, abstract, portrait, landscape, still life, surrealism, cubism, impressionism,4k, modern. Do not wrap the anser in quotes.`,
+      max_tokens: 100,
+      temperature: 0.8,
+    });
+    console.log(completion.data.choices[0].text);
+    return { body: `${completion.data.choices[0].text}` };
+  },
 });
